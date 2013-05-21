59399e50df5e78f48f3a32c282e7d356f82d0128
John Quarto-vonTivadar <jcquarto@gmail.com>
2013-05-21 18:29:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nowplaying::Application.config.secret_token = '640fed4c2dd3a104161c9b4c1b9ede7f950d39e422051f543e503fb4d073163071c621c9ae96a320da24ff6e28bd78354295ce3f1f5121ec10134f51b4b4f1a5'
